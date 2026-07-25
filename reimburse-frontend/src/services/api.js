b7701554a30bfa90ae72ad7862cd6b60c473d2d8
eltim12 <eltim2000@gmail.com>
2026-07-25 11:36:36 +0700

    update api.js

--- reimburse-frontend/src/services/api.js
+++ reimburse-frontend/src/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    // "https://reimburse-api.trimind.studio/api",
-    "http://localhost:3000/api",
+    "https://reimburse-api.trimind.studio/api",
+    // "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
